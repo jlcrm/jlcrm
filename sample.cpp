4fa508247485f91c521cb52474ff1f25876e5fbc
jlcrm <1123892070@qq.com>
2017-07-28 17:20:55 +0800

    create a git project

---
+++ sample.cpp
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: sample.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Fri 28 Jul 2017 05:16:34 PM CST
+ ************************************************************************/
+#include <iostream>
+
+int main()
+{
+        std::cout << "Hello Git!" << std::endl;
+        return 0;
+
+}
