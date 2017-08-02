1db310f0138fa47120e24893e9711c911c5686b1
haogan01 <1123892070@qq.com>
2017-08-03 00:54:18 +0800

    drgan
    修改

---
+++ application/index/controller/Menu.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+
+class Index
+{
+    /**
+     * @return \think\response\View
+     */
+    public function index()
+    {
+    	return view();
+    }    
+}
