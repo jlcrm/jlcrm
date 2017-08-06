0d1b9eac34fded41db42b9f2e6d0a91a309cb457
haogan01 <1123892070@qq.com>
2017-08-07 00:38:43 +0800

    dr.gan
    add

---
+++ application/index/controller/Wdgl.php
@@ -0,0 +1,14 @@
+<?php
+namespace app\index\controller;
+
+class Wdgl
+{
+	
+    public function index()
+    {
+    	echo "网电管理";
+    }
+    public function tempCustInfo(){
+    	return view();
+    }
+}
